6401e8471582514a1adc9346771428d13b8b8c06
ZHAO Xudong <zxdong@gmail.com>
2018-09-29 15:51:20 +0800

    fix #70 show active icon when extension active (#73)

--- dist/background.js
+++ dist/background.js
@@ -1,29 +1,38 @@
-
-let newWindow
-
-chrome.pageAction.onClicked.addListener(function (tab) {
-  // open float app window when click icon in office page
-  if (
-    tab &&
+function checkTab(tab) {
+  return tab &&
     tab.url &&
     tab.url.startsWith('https://app.hubspot.com') &&
     !tab.url.startsWith('https://app.hubspot.com/login') &&
-    !tab.url.startsWith('https://app.hubspot.com/myaccounts-beta')
+    !tab.url.startsWith('https://app.hubspot.com/myaccounts-beta') &&
+    !tab.url.startsWith('https://app.hubspot.com/developer')
+}
+
+async function cb(tabId) {
+  let tab = await new Promise((resolve) => {
+    chrome.tabs.get(tabId, resolve)
+  })
+  if (
+    checkTab(tab)
+  ) {
+    chrome.pageAction.show(tab.id)
+    return
+  }
+}
+
+chrome.tabs.onCreated.addListener(cb)
+chrome.tabs.onUpdated.addListener(cb)
+
+chrome.pageAction.onClicked.addListener(function (tab) {
+  chrome.pageAction.show(tab.id)
+  if (
+    checkTab(tab)
   ) {
     // send message to content.js to to open app window.
     chrome.tabs.sendMessage(tab.id, { action: 'openAppWindow' }, function(response) {
       console.log(response)
     })
-    chrome.pageAction.show(tab.id)
     return
   }
 })
 
-
-
-chrome.windows.onRemoved.addListener(function (id) {
-  if (newWindow && newWindow.id === id) {
-    newWindow = null
-  }
-})
 
