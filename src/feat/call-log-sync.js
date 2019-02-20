f2fa3a32c572e50a54842b848e8b5b7c3f44ea76
ZHAO Xudong <zxdong@gmail.com>
2019-02-20 09:16:03 +0800

    Support missed call log

--- src/feat/call-log-sync.js
+++ src/feat/call-log-sync.js
@@ -56,10 +56,10 @@ function notifySyncSuccess({
 }
 
 export async function syncCallLogToThirdParty(body) {
-  let result = _.get(body, 'call.result')
-  if (result !== 'Call connected') {
-    return
-  }
+  // let result = _.get(body, 'call.result')
+  // if (result !== 'Call connected') {
+  //   return
+  // }
   let isManuallySync = !body.triggerType
   let isAutoSync = body.triggerType === 'callLogSync'
   if (!isAutoSync && !isManuallySync) {
