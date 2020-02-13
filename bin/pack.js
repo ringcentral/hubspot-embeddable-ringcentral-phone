683bcc22543f829ca1b03618e3948072e2fc8a96
ZHAO Xudong <zxdong@gmail.com>
2020-02-13 17:58:51 +0800

    Add pack script

--- bin/pack.js
+++ bin/pack.js
@@ -1,3 +1,34 @@
 /**
  * pack dist folders to zip file for release
  */
+
+const {
+  exec,
+  rm
+} = require('shelljs')
+const pack = require('../package.json')
+
+// zip -vr folder.zip folder/ -x "*.DS_Store"
+// hubspot-embeddable-ringcentral-phone-google-chrome-1.6.0.zip
+// hubspot-embeddable-ringcentral-phone-mozilla-firefox-1.8.4.zip
+
+const config = [
+  {
+    name: 'google-chrome',
+    folder: 'dist'
+  },
+  {
+    name: 'mozilla-firefox',
+    folder: 'dist-firefox'
+  }
+]
+
+async function run () {
+  rm('-rf', '*.zip')
+  for (let v of config) {
+    let cmd = `zip -vr ${pack.name}-${v.name}-${pack.version}.zip ${v.folder}/ -x "*.DS_Store"`
+    exec(cmd)
+  }
+}
+
+run()
