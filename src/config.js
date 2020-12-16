209a23bd014f8c7a9b4ae8a38ff735e17471c0d2
ZHAO Xudong <zxdong@gmail.com>
2020-12-16 08:37:13 +0800

    fix: Fix inbound call popup contact info function

--- src/config.js
+++ src/config.js
@@ -390,7 +390,7 @@ export async function thirdPartyServiceConfig (serviceName) {
         sessionIds
       })
     } else if (
-      type === 'rc-active-call-notify'
+      type === 'rc-call-ring-notify'
     ) {
       showContactInfoPanel(call)
     } else if (type === 'rc-region-settings-notify') {
