743c7e3e2adebc399bbd1fec072e9ec2e7f14da2
ZHAO Xudong <zxdong@gmail.com>
2021-08-26 11:33:55 +0800

    Reuse fetch contact request, reduce api use

--- src/common/contact.js
+++ src/common/contact.js
@@ -3,6 +3,14 @@
  */
 
 import request from './request'
+import { notification } from 'antd'
+import {
+  getCache,
+  setCache
+} from 'ringcentral-embeddable-extension-common/src/common/cache'
+
+const time = 5000
+const prefix = 'rc-contact-cache-'
 
 export async function createContact (data) {
   const url = '/hs/create-contact'
@@ -13,12 +21,35 @@ export async function createContact (data) {
 }
 
 export async function getContact (vid) {
+  const cid = prefix + vid
+  const cached = await getCache(cid)
+  if (cached) {
+    console.log('use contact fetch cache')
+    return cached
+  }
   const url = '/hs/get-contact'
-  const r = await request(url, {
-    id: vid
-  })
+  let r = null
+  if (window.contactRequestId === vid && window.contactRequest) {
+    console.log('fetching, reuse promise')
+    r = await window.contactRequest
+  } else {
+    window.contactRequestId = vid
+    window.contactRequest = request(url, {
+      id: vid
+    })
+    r = await window.contactRequest
+  }
+  delete window.contactRequestId
+  delete window.contactRequest
   if (r && r.result && r.result.id) {
+    await setCache(cid, r.result, time)
     return r.result
+  } else if (r && r.error) {
+    notification.warn({
+      title: 'Error',
+      description: r.note,
+      duration: 10
+    })
   } else {
     console.log(r)
     return null
